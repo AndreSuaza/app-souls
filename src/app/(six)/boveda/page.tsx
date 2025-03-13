7bec9d193b1668390ce5ef225cf68ebe6ef7a19f
AsuazaDyN <asuaza@dichter-neira.com>
2025-03-13 18:11:07 -0500

    fix 5

--- src/app/(six)/boveda/page.tsx
+++ src/app/(six)/boveda/page.tsx
@@ -8,7 +8,7 @@ interface Props {
     page?: string;
     products?: string;
     rarities?: string;
-    orden: string;
+    orden?: string;
   }
 }
 
@@ -35,11 +35,11 @@ export const metadata: Metadata = {
 
 export default async function BovedaPage({ searchParams }: Props) {
 
-  const { products,  page, rarities, orden } = await searchParams;
+  const { products, page, rarities } = await searchParams;
   const page2 = page ? parseInt( page ) : 1 
 
   const propertiesCards = await getPropertiesCards();
-  const { cards, totalPage } = await getPaginatedPricesCards({ page: page2, products, rarities, orden });
+  const { cards, totalPage } = await getPaginatedPricesCards({ page: page2, products, rarities });
 
   return (
     <>
