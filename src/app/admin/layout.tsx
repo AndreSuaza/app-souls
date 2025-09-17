97e7673941d5bcffe7fdf6ec3547265da108b47c
AsuazaDyN <asuaza@dichter-neira.com>
2025-09-17 10:51:20 -0500

    fix import components

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,5 @@
 import { Sidebar, TopMenu } from "@/components";
-import { Footer } from "@/components/ui/footer/Footer";
+import { Footer } from "@/components";
 import moment from 'moment';
 import 'moment/locale/es';
 
