c140d37637777abec9f8e3290af930f65f829870
AsuazaDyN <asuaza@dichter-neira.com>
2026-01-22 21:06:20 -0500

    cambio de numero tiendas

--- src/app/(six)/productos/layout.tsx
+++ src/app/(six)/productos/layout.tsx
@@ -17,7 +17,7 @@ export default function SixLayout({
           <Link href="/tiendas" >
           <button className="flex mx-auto items-center ml-2 px-2 py-1 border-2 rounded-lg hover:bg-yellow-600"><IoStorefrontOutline className='w-6 h-6 md:mr-2'/><span className='hidden md:block'>Tiendas oficiales</span></button>
           </Link>
-          <Link href="https://wa.me/573180726340?text=Hola%2C+me+gustar%C3%ADa+conocer+m%C3%A1s+de+sus+productos" >
+          <Link href="https://wa.me/573125580644?text=Hola%2C+me+gustar%C3%ADa+conocer+m%C3%A1s+de+sus+productos" >
           <button className="flex flex-row ml-4 px-2 py-1 border-2 rounded-lg hover:bg-yellow-600"><IoLogoWhatsapp  className='w-6 h-6 md:mr-1'/><span className='hidden md:block'>Escríbenos</span></button>
           </Link>
         </div> 
